af40a9a5d54643dce77eba0b5fb88289c4012267
Tea <cha9n.1488@gmail.com>
2025-09-25 19:07:00 +0300

    5

--- src/main/java/com/example/boolstore2/entity/Book.java
+++ src/main/java/com/example/boolstore2/entity/Book.java
@@ -20,10 +20,13 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String author;
+
     @Column(nullable = false, unique = true)
     private String isbn;
 
